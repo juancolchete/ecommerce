183a67a8de8bd08b51ac8fac68a7e557f4626b34
Juan Colchete <juanudkp@gmail.com>
2019-11-23 20:00:19 -0300

    Commit Inicial

---
+++ recursos/sp_addresses_save.sql
@@ -0,0 +1,41 @@
+DELIMITER $$
+CREATE PROCEDURE `sp_addresses_save`(
+pidaddress int(11), 
+pidperson int(11),
+pdesaddress varchar(128),
+pdescomplement varchar(32),
+pdescity varchar(32),
+pdesstate varchar(32),
+pdescountry varchar(32),
+pdeszipcode char(8),
+pdesdistrict varchar(32)
+)
+BEGIN
+
+	IF pidaddress > 0 THEN
+		
+		UPDATE tb_addresses
+        SET
+			idperson = pidperson,
+            desaddress = pdesaddress,
+            descomplement = pdescomplement,
+            descity = pdescity,
+            desstate = pdesstate,
+            descountry = pdescountry,
+            deszipcode = pdeszipcode, 
+            desdistrict = pdesdistrict
+		WHERE idaddress = pidaddress;
+        
+    ELSE
+		
+		INSERT INTO tb_addresses (idperson, desaddress, descomplement, descity, desstate, descountry, deszipcode, desdistrict)
+        VALUES(pidperson, pdesaddress, pdescomplement, pdescity, pdesstate, pdescountry, pdeszipcode, pdesdistrict);
+        
+        SET pidaddress = LAST_INSERT_ID();
+        
+    END IF;
+    
+    SELECT * FROM tb_addresses WHERE idaddress = pidaddress;
+
+END$$
+DELIMITER ;
