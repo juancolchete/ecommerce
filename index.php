6a6e06f16fd4e42174ebf0bafb731b7e316a2035
Juan Colchete <juanudkp@gmail.com>
2019-11-23 20:59:08 -0300

    Criação da classe Page

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \juanudk\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function () {
 
-	$sql = new juanudk\DB\Sql();
+	$page = new Page();
 
-	$results = $sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
+	$page->setTpl("index");
 });
 
 $app->run();
