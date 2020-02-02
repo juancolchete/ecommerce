dfa57b59bf80a6346b223b03dac074fcca2a6ad5
juanudk <juanudkp@gmail.com>
2020-02-02 08:27:36 -0300

    Admin - Categorias - CRUD

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use \Slim\Slim;
 use \juanudk\Page;
 use \juanudk\PageAdmin;
 use \juanudk\Model\User;
+use \juanudk\Model\Category;
 
 $app = new Slim();
 
@@ -220,5 +221,90 @@ $app->post("/admin/forgot/reset", function(){
 	
 });
 
+$app->get("/admin/categories", function(){
+
+	User::verifyLogin();
+	
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories",[
+		'categories'=>$categories
+	]);
+	
+
+});
+
+$app->get("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+});
+
+$app->post("/admin/categories/create", function(){
+	
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header('Location: /admin/categories');
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update",[
+		'category'=>$category->getValues()
+	]);
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
+
+});
 $app->run();
 ?>
