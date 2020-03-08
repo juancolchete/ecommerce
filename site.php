4cec044c0cadee67a78f481071258613951514e7
juanudk <juanudkp@gmail.com>
2020-03-08 10:57:17 -0300

    Site Carrinho de Compras Calcular Frete

--- site.php
+++ site.php
@@ -65,7 +65,8 @@ $app->get("/cart",function(){
 
 	$page->setTpl("cart", [
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts()
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
 	]);
 });
 
@@ -118,5 +119,15 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 	header("Location: /cart");
 	exit;
 	
+});
+
+$app->post("/cart/freight", function(){
+	$cart = Cart::getFromSession();
+	
+	$cart->setFreight($_POST['zipcode']);
+
+	header("Location: /cart");
+
+	exit;
 });
 ?>
